e8bf76609b75f80c6ccbb82f0cd16d1e12e14c8b
yossatron <yossatron@yossatrons-macbook-pro.local>
2018-04-05 10:18:10 +0700

    Initial Commit

---
+++ ClickMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClickMe
+//
+//  Created by yossatron on 5/4/2561 BE.
+//  Copyright © 2561 yossatron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ClickMeTests/ClickMeTests.swift
@@ -0,0 +1,36 @@
+//
+//  ClickMeTests.swift
+//  ClickMeTests
+//
+//  Created by yossatron on 5/4/2561 BE.
+//  Copyright © 2561 yossatron. All rights reserved.
+//
+
+import XCTest
+@testable import ClickMe
+
+class ClickMeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
